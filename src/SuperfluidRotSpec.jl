45561d7dbed156c8897e7949b1a3a90eaa2e1517
CatGD <53821518+GDufenshuoo@users.noreply.github.com>
2023-03-11 21:41:23 +0800

    beta_0.0.1

--- src/SuperfluidRotSpec.jl
+++
@@ -1,9 +0,0 @@
-module SuperfluidRotSpec
-
-using ApproxFun
-using FLoops
-# using Tullio
-
-greet() = print("Hello World!")
-
-end # module SuperfluidRotSpec
--- ver01/Pos.jl
+++
@@ -1,7 +0,0 @@
-function U(x)
-    return 0.0
-end
-
-function Uᴬ(x)
-    return 0.0
-end
